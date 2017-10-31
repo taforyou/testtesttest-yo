1d26897e00d24d49632dc5aaaa52e27e048b753c
taforyou <taforyou@hotmail.com>
2017-10-31 08:17:09 +0700

    ยังพังอยู่

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -124,6 +124,7 @@ export default class Navbar extends Component {
       <AppBar
         title={
           <Link to={authExists ? LIST_PATH : '/'} className={classes.brand}>
+            {/* ยังพังอยู่ */}
             <img src={require('../images/test.png')} />Boardgame Tournament
           </Link>
         }
